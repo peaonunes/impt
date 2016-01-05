4d60c712cf45025cb54d223b11162a35c3518119
Valbrand <daniel@dvalbrand.com>
2016-01-05 20:15:34 -0300

    Finished LZ77 algorithm implementation

--- src/compression/lz77.cpp
+++ src/compression/lz77.cpp
@@ -1,28 +1,17 @@
 #include <cstdio>
-#include <cstdint>
 #include <cstring>
 #include <cstdlib>
 
-void encode_pointer(uint16_t jump, uint8_t length, uint8_t *code_ptr1, uint8_t *code_ptr2) {
+#include "lz77.h"
+
+static inline void encode_pointer(uint16_t jump, uint8_t length, uint8_t *code_ptr1, uint8_t *code_ptr2) {
 	(*code_ptr1) = ((0x0ff0 & jump) >> 4);
 	(*code_ptr2) = ((0x000f & jump) << 4) | (length & 0x000f);
 }
 
-void decode_pointer(uint8_t code_ptr1, uint8_t code_ptr2, uint16_t *jump, uint8_t *length) {
+static inline void decode_pointer(uint8_t code_ptr1, uint8_t code_ptr2, uint16_t *jump, uint8_t *length) {
 	(*jump) = ((code_ptr1 << 4) | (code_ptr2 >> 4));
 	(*length) = code_ptr2 & 0x000f;
-}
-
-void print_array(uint8_t *array, int length) {
-	uint16_t jump;
-	uint8_t match_len;
-
-	printf("\n");
-	for (int i = 0; i < length;) {
-		decode_pointer(array[i++], array[i++], &jump, &match_len);
-		printf("%u %u '%c'\n", jump, match_len, array[i++]);
-	}
-	printf("\n");
 }
 
 void prefix_match(char* text, int txtlen, int cursor, int Ls, int Ll, uint16_t* jump, uint8_t* length) {
@@ -93,33 +82,16 @@ void lz77_decode(uint8_t *code, int codelen, int Ls, int Ll, char* txt) {
 	int text_index = 0;
 
 	while (i < codelen) {
-		decode_pointer(code[i++], code[i++], &jump, &match_length);
+		decode_pointer(code[i], code[i + 1], &jump, &match_length);
 
 		j = text_index - jump;
 		while (match_length--) {
 			txt[text_index++] = txt[j++];
 		}
-		txt[text_index++] = code[i++];
+		txt[text_index++] = code[i + 2];
+
+		i += 3;
 	}
 	if (txt[text_index] != '\0')
 		txt[++text_index] = '\0';
-}
-
-int main() {
-	char* txt = "carai";
-	int txtlen = strlen(txt);
-	char* decoded_text = (char*)malloc((txtlen+1)*sizeof(char));
-	int search_window_length = 1 << 12;
-	int lookahead_length = 1 << 4;
-	int codelen = 0;
-
-	uint8_t* encoded_text = lz77_encode(txt, txtlen, search_window_length, lookahead_length, &codelen);
-
-	print_array(encoded_text, codelen);
-
-	lz77_decode(encoded_text, codelen, search_window_length, lookahead_length, decoded_text);
-
-	printf("decoded text: %s\n", decoded_text);
-	printf("text length: %d, resulting length: %d\n", txtlen, (codelen * 3));
-	printf("strcmp: %d\n", strcmp(txt, decoded_text));
 }
---
+++ src/compression/lz77.h
@@ -0,0 +1,9 @@
+#ifndef LZ77_H
+#define LZ77_H
+
+#include <cstdint>
+
+uint8_t* lz77_encode(char* txt, int txtlen, int Ls, int Ll, int* code_length);
+void lz77_decode(uint8_t *code, int codelen, int Ls, int Ll, char* txt);
+
+#endif
